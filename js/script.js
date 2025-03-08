947c5304441a899db2bc0d586113102fb709246a
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-03-08 14:23:20 -0500

    added triangle drawing

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 /** 
-Author: Goshanraj Govindaraj (#) & Arkel Ziko (#400586552)
+Author: Goshanraj Govindaraj (400599969) & Arkel Ziko (#400586552)
 Date: Thursday, March 6, 2025 at 8:35 AM
 Description: The JS consisting of all Canvas Drawing Functionallity
              and logic for persisting drawings
@@ -7,61 +7,88 @@ Description: The JS consisting of all Canvas Drawing Functionallity
 
 // NOTE: NEED TO FIGURE OUT HOW TO USE LOCAL STORAGE FOR THIS
 window.addEventListener("load", function () {
+  let c = document.getElementById("canvas");
+  let ctx = c.getContext("2d");
+  let clear = document.getElementById("clear-button");
+  let undo = document.getElementById("undo-button");
 
-    let c = document.getElementById("canvas");
-    let ctx = c.getContext("2d");
-    let clear = document.getElementById("clear-button");
-    let undo = document.getElementById("undo-button");
-
-    class Circle {
-        constructor(x, y, radius, color) {
-            this.x = x;
-            this.y = y
-            this.radius = radius;
-            this.color = color;
-        }
-
-        draw(ctx) {
-            ctx.beginPath();
-            ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-            ctx.fillStyle = this.color;
-            ctx.fill();
-            ctx.closePath();
-        }
+  class Circle {
+    constructor(x, y, radius, color) {
+      this.x = x;
+      this.y = y;
+      this.radius = radius;
+      this.color = color;
     }
 
-    function getMousePosition(event) {
-        const rect = c.getBoundingClientRect();
-        const x = event.clientX - rect.left; 
-        const y = event.clientY - rect.top;
-        return { x, y };
+    draw(ctx) {
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+      ctx.fillStyle = this.color;
+      ctx.fill();
+      ctx.closePath();
+    }
+  }
+
+  class Triangle {
+    constructor(x, y, height, color) {
+      this.x = x;
+      this.y = y;
+      this.height = height;
+      this.color = color;
     }
 
-    c.addEventListener("click", (event) => {
-        let shape = document.getElementById("user-shape").value;
-        let color = document.getElementById("color-input").value;
-        let size = parseInt(document.getElementById("size-input").value);
-        let { x, y } = getMousePosition(event)
+    draw(ctx) {
+      const base = (2 * this.height) / Math.sqrt(3); //formula for the base using the 30/60/90 formula for equaliterals
+      const x1 = this.x;
+      const y1 = this.y;
 
-        if (shape === "circle") {
-            const circle = new Circle(x, y, size / 2, color);
-            circle.draw(ctx);
-        }
-        else if (shape === "square") {
-            // DRAW SQUARE LOGIC
-        }
-        else {
-            /// THIS LAST ONE IS TRIANGLE LOGIC
-        }
+      const x2 = this.x - base / 2;
+      const y2 = this.y + this.height;
 
-    });
+      const x3 = this.x + base / 2;
+      const y3 = this.y + this.height;
 
-    undo.addEventListener("click", function () {
-        //Undo logic of most recent shape made
-    });
+      ctx.beginPath();
+      ctx.lineTo(x1, y1);
+      ctx.lineTo(x2, y2);
+      ctx.lineTo(x3, y3);
+      ctx.closePath();
 
-    clear.addEventListener("click", function () {
-        ctx.clearRect(0, 0, c.width, c.height);
-    });
+      ctx.fillStyle = this.color;
+      ctx.fill();
+      ctx.stroke();
+    }
+  }
 
+  function getMousePosition(event) {
+    const rect = c.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    return { x, y };
+  }
+
+  c.addEventListener("click", (event) => {
+    let shape = document.getElementById("user-shape").value;
+    let color = document.getElementById("color-input").value;
+    let size = parseInt(document.getElementById("size-input").value);
+    let { x, y } = getMousePosition(event);
+
+    if (shape === "circle") {
+      const circle = new Circle(x, y, size / 2, color);
+      circle.draw(ctx);
+    } else if (shape === "square") {
+      // DRAW SQUARE LOGIC
+    } else {
+      const triangle = new Triangle(x, y, size, color);
+      triangle.draw(ctx);
+    }
+  });
+
+  undo.addEventListener("click", function () {
+    //Undo logic of most recent shape made
+  });
+
+  clear.addEventListener("click", function () {
+    ctx.clearRect(0, 0, c.width, c.height);
+  });
 });
